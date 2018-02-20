c312573b62b0f3edf9decf163c3cc3e015592965
Makayla Moster <monkmonster96@gmail.com>
2018-02-19 22:25:09 -0500

    Commit Three.
    Removal of excess code not related to Fractal Tree.

--- Makayla/HelloWorld.cpp
+++
@@ -1,7 +0,0 @@
-//g++ -o Makayla.exe HelloWorld.cpp libglew32.dll.a libglfw3dll.a -I include -lOpenGL32 -L ./ -lglew32 -lglfw3
-#include <iostream>
-using namespace std;
-
-int main(){
-cout << "Michael Loves Makayla!" << endl;
-}
